d9b86ff59fae545fe27331763ca14d60965b423c
Andrei Grini <Andrei.Grini@avanssoma.no>
2022-02-25 23:23:29 +0200

    products component init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavigationMenuComponent } from './components/top-navigation-menu/top-navigation-menu.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopNavigationMenuComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TopNavigationMenuComponent, ProductsComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import productsData from '../../../assets/products.json';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent implements OnInit {
+  constructor() {}
+
+  products: Product[] = productsData;
+
+  ngOnInit(): void {}
+}
